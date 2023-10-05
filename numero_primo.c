5417bf328e741bc0a02ba3c76e1077e9f2aaa143
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-05 16:25:45 -0500

    Variable y mensaje

--- numero_primo.c
+++ numero_primo.c
@@ -1,3 +1,10 @@
 /*Construir un programa en lenguaje C que calcule si un número entero 
 ingresado por el usuario es un número primo.*/
 //Mateo Cisneros
+#include <stdio.h>
+
+main(){
+    int num;
+    printf("Introduce un numero -> ");
+    scanf("%d", &num);
+}
