1163b51f1cdf3363d8d03daba4a3db2421a9e9e5
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-05 16:41:29 -0500

    Ultimo commit y version final

--- numero_primo.c
+++ numero_primo.c
@@ -11,6 +11,17 @@ main(){
     if (num == 0 || num == 1)
     {
         printf("%d no es un numero primo.\n", num);
+        return 0;
     }
-    
+
+    for (int i = 2; i * i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf("%d no es un numero primo.\n", num);
+            return 0;
+        }
+    }
+    printf("%d es un numeros primo.\n", num);
+    return 0;
 }
